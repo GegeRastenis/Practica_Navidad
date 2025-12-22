e3f0cdc9d581c5dcd82a0927d4de8dd32bb4c6ae
Gegerastenis <giselle_rastenis@hotmail.com>
2025-12-22 14:18:47 -0300

    Subiendo ejercicios clase8 a 11

---
+++ clase10/miRepoNavidenio.js
@@ -0,0 +1,7 @@
+/*Clase 10: Git y GitHub
+Esta clase fue clave para aprender a subir nuestro código a la nube.
+Ejercicio: ¡Mi Repo Navideño! Este no es de código, ¡es de gestión!
+1.	Creá una carpeta en tu compu llamada Practica_Navidad.
+2.	Hacé todos estos ejercicios adentro de esa carpeta.
+3.	Creá un repositorio nuevo en GitHub.
+4.	Subí tus ejercicios. ¡Es la mejor forma de demostrar que practicaste!*/
